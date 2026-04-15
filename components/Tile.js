37978a6b2486fa64659f10c074825dfd79a117b8
Bandit8238 <zech.sq@gmail.com>
2026-04-14 20:31:10 -0600

    Tile.js

--- components/Tile.js
+++ components/Tile.js
@@ -1,1 +1,9 @@
+export function createTile(game) {
+  const div = document.createElement("div");
+  div.className = "tile";
+  div.innerText = game.name;
 
+  div.onclick = () => window.open(game.url);
+
+  return div;
+}
